ac2fe6339dce79291532e73c6a3238e99b90d80e
Ben Alman <hi@benalman.me>
2016-04-10 18:18:52 -0500

    General layout complete, menu button working, responsive menu and nav implemented.

---
+++ js/uiHandlers.js
@@ -0,0 +1,26 @@
+$( document ).ready(function() {
+	var menuOpen = false;
+	$('#menu').click(function() {
+		if (menuOpen) {
+			menuOpen = false;
+			$(this).removeClass("click");
+			$("nav, #navMask").removeClass("open");
+		} else {
+			menuOpen = true;
+			$(this).addClass("click");
+			$("nav, #navMask").addClass("open");
+		}
+	});
+	
+	$('#navMask').click(function() {
+		if (menuOpen) {
+			menuOpen = false;
+			$("#menu").removeClass("click");
+			$("nav, #navMask").removeClass("open");
+		} else {
+			menuOpen = true;
+			$("menu").addClass("click");
+			$("nav, #navMask").addClass("open");
+		}
+	});
+});
