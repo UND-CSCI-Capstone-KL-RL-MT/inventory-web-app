6c3cee9f2f6e57cb60fb9b406a70855d2f3c96b8
Ben Alman <hi@benalman.me>
2016-04-14 22:29:14 -0500

    Added users service with ability to add a user, hooked up add user button.

--- js/app/users.js
+++ js/app/users.js
@@ -1,6 +1,32 @@
 var app = angular.module('app');
 
-app.controller('Users', function($rootScope, $scope, $timeout) {
+app.factory('UsersService', function($http) {
+	
+	return {
+		
+		getUsers: function() {
+			return $http.get('../inventory-api/get_users.php')
+				.then(function(res) {
+					return res.data;
+				}, function(res) {
+					// failure
+				})
+		},
+		
+		addUser: function(user) {
+			return $http.post('../inventory-api/add_user.php', user)
+				.then(function(res) {
+					return res.data;
+				}, function(res) {
+					// failure
+				})
+		}
+		
+	}
+	
+});
+
+app.controller('Users', function($rootScope, $scope, $timeout, UsersService) {
 	
 	$scope.users = [];
 	$scope.newUser = {first_name: '', last_name: '', email: '', isAdmin: 0};
@@ -9,6 +35,37 @@ app.controller('Users', function($rootScope, $scope, $timeout) {
 		$scope.newUser = {first_name: '', last_name: '', email: '', isAdmin: 0};
 		$scope.newUserForm.$setPristine();
 		$scope.newUserForm.$setUntouched();
+	};
+	
+	$scope.addUser = function() {
+		UsersService.addUser($scope.newUser)
+			.then(function(res) {
+				if (res.result == "success") {
+					$mdToast.show(
+						$mdToast.simple()
+							.textContent('User was added to the system.')
+							.position('bottom right')
+							.hideDelay(3000)
+					);
+					$scope.newUser = {first_name: '', last_name: '', email: '', isAdmin: 0};
+					$scope.newUserForm.$setPristine();
+					$scope.newUserForm.$setUntouched();
+				} else {
+					$mdToast.show(
+						$mdToast.simple()
+							.textContent('User could not be added to the system, please check the form and try again.')
+							.position('bottom right')
+							.hideDelay(3000)
+					);
+				}
+			}, function(res) {
+				$mdToast.show(
+					$mdToast.simple()
+						.textContent('Could not add user to system. Please refresh and try again.')
+						.position('bottom right')
+						.hideDelay(3000)
+				);
+			});
 	};
 	
 });
