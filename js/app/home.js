48494ac3467d3873898abe15f23e2366856cfb6b
Ben Alman <benjamin.alman@und.edu>
2016-04-24 15:46:20 -0500

    Added login screen view to web application.

--- js/app/home.js
+++ js/app/home.js
@@ -1,5 +1,13 @@
 var app = angular.module('app');
 
 app.controller('Home', function($rootScope, $scope, $timeout) {
+	$rootScope.user = {
+		first_name: '',
+		last_name: '',
+		username: '',
+		password: '',
+		logged: false,
+		first: false
+	};
 	$scope.theApp = "UND inventory manager";
 });
