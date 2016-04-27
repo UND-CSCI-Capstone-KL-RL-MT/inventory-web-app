2275f0c1410525e3fc3c9015c1991711a089c4de
Ben Alman <benjamin.alman@und.edu>
2016-04-27 10:08:49 -0500

    Added login controller, auth service, updates to login screen.

---
+++ js/app/login.js
@@ -0,0 +1,38 @@
+var app = angular.module('app');
+
+app.factory('AuthService', function($http) {
+	
+	return {
+		
+		login: function(user) {
+			return $http.post('../inventory-api/auth_user.php', user)
+				.then(function(res) {
+					return res.data;
+				})
+		}
+		
+	}
+	
+});
+
+app.controller('Login', function($rootScope, $scope, $timeout, AuthService) {
+
+	$scope.login = function(user) {
+		AuthService.login(user)
+			.then(function(res) {
+				console.log(res.result);
+				if (res.result == "success") {
+					$rootScope.user.username = res.message.username;
+					$rootScope.user.password = "";
+					$rootScope.user.first_name = res.message.first_name;
+					$rootScope.user.last_name = res.message.last_name;
+					$rootScope.user.is_admin = res.message.is_admin;
+					$rootScope.user.logged = true;
+				} else {
+					$rootScope.user = {};
+					$rootScope.user.authMsg = res.message;
+				}
+			});
+	};
+	
+});
