d8a18422777ccc0612c4246e8b421b2c9edc2b9a
Ben Alman <benjamin.alman@und.edu>
2016-05-12 23:50:38 -0500

    Commented the uiHandlers file.

--- js/uiHandlers.js
+++ js/uiHandlers.js
@@ -1,6 +1,11 @@
+/**
+ * The uiHandler handles elements of the user interface
+ * including the menu button and its animation classes, as well
+ * as the mask overlay when navigation is displayed on smaller screens
+ */
 $( document ).ready(function() {
 	var menuOpen = false;
-	$('#menu').click(function() {
+	$('#menu').click(function() { // when the menu button is clicked, animate it
 		if (menuOpen) {
 			menuOpen = false;
 			$(this).removeClass("click");
@@ -12,7 +17,7 @@ $( document ).ready(function() {
 		}
 	});
 	
-	$('#navMask').click(function() {
+	$('#navMask').click(function() { // When the nav mask is clicked, hide the menu and display the main content
 		if (menuOpen) {
 			menuOpen = false;
 			$("#menu").removeClass("click");
@@ -24,7 +29,7 @@ $( document ).ready(function() {
 		}
 	});
 	
-	$('.nav-link').click(function() {
+	$('.nav-link').click(function() { // when a navigational link is clicked, hide the menu to display the view
 		if (menuOpen) {
 			menuOpen = false;
 			$("#menu").removeClass("click");
