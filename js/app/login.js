9522f17f9a41be6c9a059f6607f600ffaf903614
Ben Alman <benjamin.alman@und.edu>
2016-05-13 00:36:03 -0500

    Add reset password dialog.

--- js/app/login.js
+++ js/app/login.js
@@ -27,7 +27,7 @@ app.factory('AuthService', function($http) {
  * The Login controller controls the login screen, using the rootScope to store the user's data
  * and the localStorage service to store the user's data in the browser's local storage
  */
-app.controller('Login', function($rootScope, $scope, $timeout, AuthService, localStorageService) {
+app.controller('Login', function($rootScope, $scope, $timeout, $mdDialog, $mdMedia, $mdToast, AuthService, localStorageService) {
 
 	/**
 	 * The login function attempts to log in the specified user
@@ -59,8 +59,59 @@ app.controller('Login', function($rootScope, $scope, $timeout, AuthService, loca
 			});
 	};
 	
-	$scope.showForgotPasswordDialog = function() {
+	$scope.showForgotPasswordDialog = function(ev) {
+		// use full screen when the extra small breakpoint is reached
+		var useFullScreen = $mdMedia('xs');
 		
+		// Construct and show the material dialog
+		$mdDialog.show({
+			controller: 'ForgotPasswordDialog',
+			templateUrl: './templates/dialogs/forgot_password.html',
+			parent: angular.element(document.body),
+			targetEvent: ev,
+			clickOutsideToClose: true,
+			fullscreen: useFullScreen
+		})
+			.then(function(result) {
+				// If the user saves the new user, display a success message
+				if (result == "success") {
+					$mdToast.show(
+						$mdToast.simple()
+							.textContent('Your new password was sent to your email address.')
+							.position('bottom right')
+							.hideDelay(3000)
+					);
+					$scope.getUsers();
+				// If there was a connectivity error, display the error message
+				} else if (result == "conn-error") {
+					$mdToast.show(
+						$mdToast.simple()
+							.textContent('Could not update your password. Please refresh and try again.')
+							.position('bottom right')
+							.hideDelay(3000)
+					);
+				// There was invalid information in the form
+				} else {
+					$mdToast.show(
+						$mdToast.simple()
+							.textContent('Your email was not found or was invalid.')
+							.position('bottom right')
+							.hideDelay(3000)
+					);
+				}
+			}, function() {
+			});
+	};
+	
+});
+
+app.controller('ForgotPasswordDialog', function($rootScope, $scope, $timeout, $mdDialog, AuthService, localStorageService) {
+	
+	/**
+	 * The cancel function cancels and closes the dialog
+	 */
+	$scope.cancel = function() {
+		$mdDialog.cancel();
 	};
 	
 });
